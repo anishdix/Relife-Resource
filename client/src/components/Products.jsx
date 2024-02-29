2082f09ac96f527a7f290c030274de99daf309dc
anish dix <anishdix777@gmail.com>
2024-02-29 12:02:52 +0530

    added api routes on render

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -25,8 +25,8 @@ const Products = ({cat,filters,sort}) => {
   useEffect(()=>{
     const getProducts=async()=>{
       try{
-        const res=await axios.get(cat ? `http://localhost:5000/api/products?category=${cat}`
-        :"http://localhost:5000/api/products");
+        const res=await axios.get(cat ? `https://relife-resource.onrender.com/api/products?category=${cat}`
+        :"https://relife-resource.onrender.com/api/products");
       setProducts(res.data)
       
       }catch(err){
--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/";
+const BASE_URL = "https://relife-resource.onrender.com/api/";
 
 
 const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
